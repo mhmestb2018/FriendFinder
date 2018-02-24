534be2b0cf4518b5766b99b8fed609f6ee9392db
kahonen <kristin.ahonen@gmail.com>
2018-02-23 21:27:32 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,5 @@
+// Your survey should have 10 questions of your choosing. Each answer should be on a scale of 1 to 5 based 
+// on how much the user agrees or disagrees with a question.
+
+// Your server.js file should require the basic npm packages we've used in class: express, body-parser 
+// and path.
